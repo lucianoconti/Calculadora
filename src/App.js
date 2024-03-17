3a8f8f39e3e05d341345c92085eb63e18656de7a
luciano conti <conti98.luciano@gmail.com>
2024-03-17 18:40:29 -0300

    Se agregaron estilos y funcionalidades

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ const App = () => {
   const clickBoton = (e) => {
     setResultado(resultado.concat(e.target.value));
   };
+
+  const limpiar = (e) =>{
+    setResultado("");
+  }
+
+  const borrar = (e) =>{
+    setResultado(resultado.slice(0,resultado.length - 1));
+  }
+  const calcular = (e) =>{
+    try {
+      setResultado(eval(resultado));
+    } catch (error) {
+      setResultado("SyntaxError");
+    }
+    
+
+  }
   return (
     <>
       <div className='container'>
@@ -13,26 +30,24 @@ const App = () => {
           <input type='text' value={resultado} />
         </form>
         <div className='teclado-numerico'>
-          <button>Limpiar</button>
-          <button>C</button>
-          <button>&divide;</button>
-          <button onClick={clickBoton} value={'7'}>
-            7
-          </button>
-          <button>8</button>
-          <button>9</button>
-          <button>&times;</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>&ndash;</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button className='resaltado' id='limpiar' onClick={limpiar}>Limpiar</button>
+          <button className='resaltado' onClick={borrar}>C</button>
+          <button className='resaltado' onClick={clickBoton} value={'/'}>&divide;</button>
+          <button onClick={clickBoton} value={'7'}>7</button>
+          <button onClick={clickBoton} value={'8'}>8</button>
+          <button onClick={clickBoton} value={'9'}>9</button>
+          <button className='resaltado' onClick={clickBoton} value={'*'}>&times;</button>
+          <button onClick={clickBoton} value={'4'}>4</button>
+          <button onClick={clickBoton} value={'5'}>5</button>
+          <button onClick={clickBoton} value={'6'}>6</button>
+          <button className='resaltado' onClick={clickBoton} value={'-'}>&ndash;</button>
+          <button onClick={clickBoton} value={'1'}>1</button>
+          <button onClick={clickBoton} value={'2'}>2</button>
+          <button onClick={clickBoton} value={'3'}>3</button>
+          <button className='resaltado' onClick={clickBoton} value={'+'}>+</button>
+          <button onClick={clickBoton} value={'0'}>0</button>
+          <button className='resaltado' onClick={clickBoton} value={'.'}>.</button>
+          <button className='resaltado' id='resultado' onClick={calcular} value={'='}>=</button>
         </div>
       </div>
     </>
